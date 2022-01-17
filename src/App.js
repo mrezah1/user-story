e7308679f0959ccf99b77184a1d5c86f23b485e2
Mohammadreza <Mrhhajiyan@gmail.com>
2022-01-17 16:59:46 +0330

    added components to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Theme from "components/Theme";
+import Users from "components/Users";
+import { useTheme } from "context/ThemeProvider";
 import "./App.css";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function App() {
-  return <div></div>;
+  const { theme } = useTheme();
+  return (
+    <div className={`container ${theme}`}>
+      <Theme />
+      <h1>Users Story🤩</h1>
+      <Users />
+    </div>
+  );
 }
 
 export default App;
